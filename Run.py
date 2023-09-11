09b4243b04f3af0570ab36be225884e1667f4a69
naren0706 <narenthrakishhore@gmail.com>
2023-09-11 12:55:49 +0530

    [Narenthra Kishore] Add : create a branch push the branch and merge it with the main

---
+++ Run.py
@@ -0,0 +1,30 @@
+import subprocess
+
+def function(git_command):
+    try:
+        result = subprocess.run(git_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        # Check if the command was successful
+        if result.returncode == 0:
+            print("Command executed successfully:")
+            print(result.stdout)
+        else:
+            print("Command failed with error:")
+            print(result.stderr)
+    except Exception as e:
+        print("An error occurred:", str(e))
+        
+def createUC(UC,msg):
+    function("git checkout -b"+UC)
+    function("git add .")
+    function( "git commit -m \" [Narenthra Kishore] Add : "+msg+"\"")
+    function("git push origin "+UC)
+    function("git checkout main")
+    function("git merge "+UC)
+    function("git push origin main ")
+    return;
+        
+print("Create UC and push with a commit message and merge it with main ")
+UCname=input("Enter the UseCaseName:")
+commitmessage=input("Enter the commit message :")
+createUC(UCname,commitmessage)
